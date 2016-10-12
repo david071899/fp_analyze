ff1f81eadd0d6f3de549d473d8c79dc401f5b5b5
davy <davy0718@gmail.com>
2016-10-12 13:00:12 +0800

    fix import function

--- data_parser/views.py
+++ data_parser/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.shortcuts import  render_to_response
 
+from django.template import RequestContext
+
 from data_parser.models import Post, User, Comment
 
 import requests
