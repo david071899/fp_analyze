0099e05df49b4ed6bd48fde7c64df56b4d142f1f
davy <davy0718@gmail.com>
2016-11-19 20:53:29 +0800

    add created_at col in Term

--- text_mining/models.py
+++ text_mining/models.py
@@ -7,6 +7,7 @@ class Term (models.Model):
   value = models.CharField(max_length = 255)
   frequency_of_all_post = models.IntegerField(default = 0)
   flag = models.CharField(max_length = 255, default = '')
+  created_at = models.DateTimeField(default = '2016-11-01 12:00:00+08:00')
   related_term = models.ManyToManyField('self')
 
 
