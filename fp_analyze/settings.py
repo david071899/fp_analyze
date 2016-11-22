74a5afdc3bbb557d32c8a72a932874e41d186d59
davy <davy0718@gmail.com>
2016-11-22 23:42:10 +0800

    merge front into django

--- fp_analyze/settings.py
+++ fp_analyze/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'data_parser',
     'text_mining',
+    'front',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'fp_analyze.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [[os.path.join(BASE_DIR, 'front/templates').replace('\\', '/')]],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
--- fp_analyze/urls.py
+++ fp_analyze/urls.py
@@ -22,6 +22,8 @@ from text_mining.views import count_tf, laern_seg_rnn
 
 from api.views import all_terms
 
+from front.views import index
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^parse_all_post_content/', parse_all_post_content),
@@ -29,4 +31,5 @@ urlpatterns = [
     url(r'^text_mining/count_tf', count_tf),
     url(r'^text_mining/laern_seg_rnn', laern_seg_rnn),
     url(r'^api/all_terms', all_terms),
+    url(r'^index', index),
 ]
