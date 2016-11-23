db05c0af60f974ec51e667cf95ab7a42b9fd42c4
davy <davy0718@gmail.com>
2016-11-23 13:25:33 +0800

    shift frequency

--- api/views.py
+++ api/views.py
@@ -10,13 +10,18 @@ from text_mining.models import Term
 
 def all_terms(requrest):
 
-  data = Term.objects.extra(where=["CHAR_LENGTH(value) > 1"])
+  data = Term.objects.filter(Q(flag = 'n') | Q(flag = 'nz') | Q(flag = 'nr') | Q(flag = 'ns') )
 
-  data = data.filter(Q(flag = 'n') | Q(flag = 'nz') | Q(flag = 'nr') | Q(flag = 'ns') )
+  data = data.extra(where=["CHAR_LENGTH(value) > 1"])
 
-  data = data.order_by('-frequency_of_all_post')
+  data = data.order_by('-frequency_of_all_post')[:50]
 
-  json_data = [list(x) for x in data.values_list('value', 'frequency_of_all_post')][:20]
+  shift_num = data[0].frequency_of_all_post / 200
+
+  for i in data:
+    i.frequency_of_all_post = i.frequency_of_all_post / shift_num
+
+  json_data = [list(x) for x in data.values_list('value', 'frequency_of_all_post')]
 
   response = JsonResponse(json_data, safe = False)
   response["Access-Control-Allow-Origin"] = "*"
