1d75884584c3c262f06360f4f5c1c31ddada2313
davy <davy0718@gmail.com>
2016-11-29 15:46:14 +0800

    try to fix load static

--- fp_analyze/settings.py
+++ fp_analyze/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/home/davy/fp_analyze/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/static/',
