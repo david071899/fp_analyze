ed70a2d0716235ca9988b1f671f607918765d484
davy <davy0718@gmail.com>
2016-11-29 15:50:22 +0800

    同上

--- fp_analyze/settings.py
+++ fp_analyze/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/davy/fp_analyze/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/static/',
+    '/home/davy/fp_analyze/front/static/'
     '/var/www/static/',
 ]
